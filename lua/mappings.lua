fc904e883cb2cc49d55d3eae30e836c2273bdb98
Michał Ciesielski <majalcmaj@gmail.com>
2025-12-19 02:47:51 +0100

    Some more adjustment

---
+++ lua/mappings.lua
@@ -0,0 +1,14 @@
+vim.keymap.set('n', '<leader>bD', function()
+  local current_buf = vim.api.nvim_get_current_buf()
+  for _, i in ipairs(vim.api.nvim_list_bufs()) do
+    if i ~= current_buf then
+      local type = vim.api.nvim_get_option_value('buftype', { buf = i })
+      if type ~= 'nofile' and vim.api.nvim_buf_get_option(i, 'modified') then
+        vim.api.nvim_buf_call(i, function()
+          vim.cmd 'w'
+        end)
+      end
+      vim.api.nvim_buf_delete(i, {})
+    end
+  end
+end, { desc = 'Close all buffers except current' })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,9 @@ return {
   version = '*',
   dependencies = {
     'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
+    'antosha417/nvim-lsp-file-operations',
   },
   lazy = false,
   keys = {

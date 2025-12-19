4963ae75eadcff88f0cb32d868ab3d952e698f7e
Michał Ciesielski <majalcmaj@gmail.com>
2025-12-19 02:14:12 +0100

    Configure vim tips the way I like it

--- lua/plugins/vim-tips.lua
+++ lua/plugins/vim-tips.lua
@@ -1,18 +1,19 @@
 return {
-    "saxon1964/neovim-tips",
-    version = "*",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
-        "MeanderingProgrammer/render-markdown.nvim",
-    },
-    opts = {
-        user_file = vim.fn.stdpath("config") .. "/neovim_tips/user_tips.md",
-        user_tip_prefix = "[User] ",
-        warn_on_conflicts = true,
-        daily_tip = 1,
-        bookmark_symbol = "🌟 ",
-    },
-    init = function()
-        local map = vim.keymap.set
-    end,
+  'saxon1964/neovim-tips',
+  version = '*', -- Only update on tagged releases
+  lazy = false,
+  dependencies = {
+    'MunifTanjim/nui.nvim',
+    'MeanderingProgrammer/render-markdown.nvim', -- Rich rendering with advanced features
+  },
+  opts = {
+    -- OPTIONAL: Location of user defined tips (default value shown below)
+    user_file = vim.fn.stdpath 'config' .. '/neovim_tips/user_tips.md',
+    user_tip_prefix = '[User] ',
+    warn_on_conflicts = true,
+    -- 0 = off, 1 = once per day, 2 = every startup
+    daily_tip = 1,
+    bookmark_symbol = '🌟 ',
+  },
 }
+

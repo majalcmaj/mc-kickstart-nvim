6ad9a46f5c0ee6ebace461247b905a5d2e8bad9d
Michał Ciesielski <majalcmaj@gmail.com>
2025-12-20 01:56:48 +0100

    Setup the linters nicely

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,44 +2,23 @@ return {
 
   { -- Linting
     'mfussenegger/nvim-lint',
+    dependencies = {
+      { 'mason-org/mason.nvim', opts = {} },
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+    },
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        python = { 'ruff' },
+        javascript = { 'eslint' },
+        typescript = { 'eslint' },
       }
 
-      -- To allow other plugins to add linters to require('lint').linters_by_ft,
-      -- instead set linters_by_ft like this:
-      -- lint.linters_by_ft = lint.linters_by_ft or {}
-      -- lint.linters_by_ft['markdown'] = { 'markdownlint' }
-      --
-      -- However, note that this will enable a set of default linters,
-      -- which will cause errors unless these tools are available:
-      -- {
-      --   clojure = { "clj-kondo" },
-      --   dockerfile = { "hadolint" },
-      --   inko = { "inko" },
-      --   janet = { "janet" },
-      --   json = { "jsonlint" },
-      --   markdown = { "vale" },
-      --   rst = { "vale" },
-      --   ruby = { "ruby" },
-      --   terraform = { "tflint" },
-      --   text = { "vale" }
-      -- }
-      --
-      -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
-      -- lint.linters_by_ft['json'] = nil
-      -- lint.linters_by_ft['markdown'] = nil
-      -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
-      -- lint.linters_by_ft['text'] = nil
+      local ensure_installed = vim.iter(vim.tbl_values(lint.linters_by_ft)):flatten():totable()
+      print(ensure_installed)
+      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
@@ -51,7 +30,8 @@ return {
           -- avoid superfluous noise, notably within the handy LSP pop-ups that
           -- describe the hovered symbol using Markdown.
           if vim.bo.modifiable then
-            lint.try_lint()
+            -- Ignore errors to avoid error when a linter is not installed
+            lint.try_lint(nil, { ignore_errors = true })
           end
         end,
       })
